035e8b348eee94fde24a719c8240c4c1113a1e71
Yujonpradhananga <yujonpradhan123@gmail.com>
2026-03-29 20:55:29 +0545

    learning

--- my-redis/src/main.rs
+++ my-redis/src/main.rs
@@ -1,10 +1,22 @@
-use mini_redis::{Result, client};
+use mini_redis::{Connection, Frame};
+use tokio::net::{TcpListener, TcpStream};
 
 #[tokio::main]
-async fn main() -> Result<()> {
-    let mut client = client::connect("127.0.0.1:6379").await?;
-    client.set("hello", "world".into()).await?;
-    let result = client.get("hello").await?;
-    println!("got value from the server: result={:?}", result);
-    Ok(())
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+
+    loop {
+        let (socket, _) = listener.accept().await.unwrap();
+        process(socket).await;
+    }
+}
+
+async fn process(socket: TcpStream) {
+    let mut connection = Connection::new(socket);
+
+    if let Some(frame) = connection.read_frame().await.unwrap() {
+        println!("GOT: {:?}", frame);
+        let response = Frame::Error("unimplemented".to_string());
+        connection.write_frame(&response).await.unwrap();
+    }
 }
---
+++ my-redis/target/debug/examples/hello_redis-7dab8fb0e0a0821e.d
@@ -0,0 +1,9 @@
+/home/yujon/Projects/mini-redis/my-redis/target/debug/examples/hello_redis-7dab8fb0e0a0821e.d: examples/hello-redis.rs Cargo.toml
+
+/home/yujon/Projects/mini-redis/my-redis/target/debug/examples/libhello_redis-7dab8fb0e0a0821e.rmeta: examples/hello-redis.rs Cargo.toml
+
+examples/hello-redis.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
